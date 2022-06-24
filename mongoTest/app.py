7e094121eec3589f2efef20ee310c5b7759255ad
JimenaVega <jimevega28@gmail.com>
2022-06-24 11:25:12 -0300

    Servidor mongo con interfaz de rates agregado

--- mongoTest/app.py
+++ mongoTest/app.py
@@ -14,37 +14,27 @@ pycom = db.pycom
 @app.route('/', methods=('GET', 'POST'))
 def index():
     if request.method=='POST':
-        content = request.form['content']
-        transmission_rate = request.form['transmission_rate']
-        acceleration_rate = request.form['acceleration_rate']
+        transmission_rate = request.form['transmission']
+        acc_rate = request.form['acc_rate']
         light_rate = request.form['light_rate']
-        temperature_rate = request.form['temperature_rate']
-        humidity_rate = request.form['humidity_rate']
-        altitude_rate = request.form['altitude_rate']
-        battery_voltage_rate = request.form['battery_voltage_rate']
+        temp_rate = request.form['temp_rate']
+        hum_rate = request.form['hum_rate']
+        alt_rate = request.form['alt_rate']
         roll_rate = request.form['roll_rate']
         pitch_rate = request.form['pitch_rate']
-
         degree = request.form['degree']
         pycom.insert_one({
-                            'content':content,
-                            'degree': degree
-                         })
+                          'transmission_rate':transmission_rate,
+                          'acceleration_rate':acc_rate,
+                          'light_rate':light_rate,
+                          'temperature_rate':temp_rate,
+                          'humidity_rate':hum_rate,
+                          'altitude_rate':alt_rate,
+                          'roll_rate':roll_rate,
+                          'pitch_rate':pitch_rate,
+                          'degree': degree})
         return redirect(url_for('index'))
 
-
-    # pycom.insert_one({
-    #                 'transmission_rate':transmission_rate,
-    #                 'acceleration_rate':acceleration_rate,
-    #                 'light_rate':light_rate,
-    #                 'temperature_rate':temperature_rate,
-    #                 'humidity_rate':humidity_rate,
-    #                 'altitude_rate':altitude_rate,
-    #                 'battery_voltage_rate':battery_voltage_rate,
-    #                 'roll_rate':roll_rate,
-    #                 'pitch_rate':pitch_rate,
-    #                 'degree': degree
-    #                 })
-
     all_pycom = pycom.find()
-    return render_template('index.html', pycom=all_pycom)
+    return render_template('index.html', todos=all_pycom)
+
